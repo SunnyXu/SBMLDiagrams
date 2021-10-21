ed3b66a6ea7854e4ed68ee7f3045dd414cb76ee5
Jin <jin.xu.2010@gmail.com>
2021-10-20 17:10:44 -0700

    Create test_exportSBML_via_importSBML.py

---
+++ tests/test_exportSBML_via_importSBML.py
@@ -0,0 +1,198 @@
+import unittest
+from src import importSBML
+from src import exportSBML
+import pandas as pd
+
+IGNORE_TEST = False
+
+#############################
+# Tests
+#############################
+class TestKineticLaw(unittest.TestCase):
+
+  def setUp(self):
+    reactionLineType = 'bezier' #'linear' or 'bezier'
+    complexShape = '' #'' or 'monomer' or 'dimer' or 'trimer' or 'tetramer'
+    df_CompartmentData = pd.read_csv('CompartmentData.csv') 
+    df_NodeData = pd.read_csv('NodeData.csv')
+    df_ReactionData = pd.read_csv('ReactionData.csv')
+    sbmlStr_layout_render = exportSBML.main(df_CompartmentData, df_NodeData, df_ReactionData)
+    self.df_CompartmentData, self.df_NodeData, self.df_ReactionData = importSBML.main(sbmlStr_layout_render, reactionLineType, complexShape)
+
+  def testCompartment1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    test = all(item in self.df_CompartmentData.columns for item in importSBML.COLUMN_NAME_df_CompartmentData)
+    self.assertTrue(test)
+
+  def testCompartment2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    self.assertTrue(len(self.df_CompartmentData.index)>0) 
+
+  def testCompartment3(self):
+    # Test column 'net_idx' and 'idx' of df_CompartmentData are integers
+    if IGNORE_TEST:
+      return    
+    list_compartment = []
+    list_compartment += self.df_CompartmentData[importSBML.COLUMN_NAME_df_CompartmentData[0]].tolist()
+    list_compartment += self.df_CompartmentData[importSBML.COLUMN_NAME_df_CompartmentData[1]].tolist()
+    test = all(isinstance(item, int) for item in list_compartment)
+    self.assertTrue(test)
+
+  def testCompartment4(self):
+    # Test column 'id' of df_CompartmentData is string
+    if IGNORE_TEST:
+      return    
+    list_compartment = []
+    list_compartment += self.df_CompartmentData[importSBML.COLUMN_NAME_df_CompartmentData[2]].tolist()
+    test = all(isinstance(item, str) for item in list_compartment)
+    self.assertTrue(test)
+
+  def testCompartment5(self):
+    # Test column 'position' 'size' 'fill color' 'border color' of df_CompartmentData are lists
+    if IGNORE_TEST:
+      return    
+    list_compartment = []
+    for i in range(3,7):
+      list_compartment += self.df_CompartmentData.iloc[:,i].tolist()
+    test = all(isinstance(item, list) for item in list_compartment)
+    self.assertTrue(test)
+
+  def testCompartment6(self):
+    # Test column 'border width' of df_CompartmentData is a floating number
+    if IGNORE_TEST:
+      return    
+    list_compartment = []
+    list_compartment += self.df_CompartmentData[importSBML.COLUMN_NAME_df_CompartmentData[7]].tolist()
+    test = all(isinstance(item, float) for item in list_compartment)
+    self.assertTrue(test)
+
+
+  def testNode1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    test = all(item in self.df_NodeData.columns for item in importSBML.COLUMN_NAME_df_NodeData)
+    self.assertTrue(test)
+
+  def testNode2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    self.assertTrue(len(self.df_NodeData.index)>0) 
+
+  def testNode3(self):
+    # Test column 'net_idx' 'comp_idx' 'idx' 'original_idx' and 'shape_idx' of df_NodeData 
+    # are integers
+    if IGNORE_TEST:
+      return    
+    list_node = []
+    for i in range(0,3):
+      list_node += self.df_NodeData.iloc[:,i].tolist()
+    list_node += self.df_NodeData.iloc[:,9].tolist()
+    test = all(isinstance(item, int) for item in list_node)
+    self.assertTrue(test)
+
+  def testNode4(self):
+    # Test column 'position' 'size' 'txt_position' 'txt_size' 'fill color' 'border color'
+    #  'txt_font_color' of df_NodeData are lists
+    if IGNORE_TEST:
+      return    
+    list_node = []
+    for i in range(7,9):
+      list_node += self.df_NodeData.iloc[:,i].tolist()
+    for i in range(10,14):
+      list_node += self.df_NodeData.iloc[:,i].tolist()
+    list_node += self.df_NodeData.iloc[:,15].tolist()
+    test = all(isinstance(item, list) for item in list_node)
+    self.assertTrue(test)
+
+  def testNode5(self):
+    # Test column 'id' and 'floating node' of df_NodeData is str
+    if IGNORE_TEST:
+      return    
+    list_node = []
+    list_node += self.df_NodeData[importSBML.COLUMN_NAME_df_NodeData[4]].tolist()
+    list_node += self.df_NodeData[importSBML.COLUMN_NAME_df_NodeData[5]].tolist()
+    test = all(isinstance(item, str) for item in list_node)
+    self.assertTrue(test)
+
+  def testNode6(self):
+    # Test column 'concentration', 'border width' and 'txt_line_width' of df_NodeData 
+    # are floating numbers
+    if IGNORE_TEST:
+      return    
+    list_node = []
+    list_node += self.df_NodeData[importSBML.COLUMN_NAME_df_NodeData[6]].tolist()
+    list_node += self.df_NodeData[importSBML.COLUMN_NAME_df_NodeData[14]].tolist()
+    list_node += self.df_NodeData[importSBML.COLUMN_NAME_df_NodeData[16]].tolist()
+    test = all(isinstance(item, float) for item in list_node)
+    self.assertTrue(test)
+
+  def testReaction1(self):
+    # Test all the column names
+    if IGNORE_TEST:
+      return    
+    test = all(item in self.df_ReactionData.columns for item in importSBML.COLUMN_NAME_df_ReactionData)
+    self.assertTrue(test)
+
+  def testReaction2(self):
+    # Test whether there is at least one row
+    if IGNORE_TEST:
+      return    
+    self.assertTrue(len(self.df_ReactionData.index)>0) 
+
+  def testReaction3(self):
+    # Test column 'net_idx' 'idx' of df_ReactionData are integers
+    if IGNORE_TEST:
+      return    
+    list_reaction = []
+    for i in range(0,2):
+      list_reaction += self.df_ReactionData.iloc[:,i].tolist()
+    test = all(isinstance(item, int) for item in list_reaction)
+    self.assertTrue(test)
+
+  def testReaction4(self):
+    # Test column 'sources' 'targets' 'modifiers' 'fill color' 'center_position' 'handles' 
+    # of df_ReactionData are lists
+    if IGNORE_TEST:
+      return    
+    list_reaction = []
+    for i in range(3,5):
+      list_reaction += self.df_ReactionData.iloc[:,i].tolist()
+    for i in range(6,8):
+      list_reaction += self.df_ReactionData.iloc[:,i].tolist()
+    for i in range(9,11):
+      list_reaction += self.df_ReactionData.iloc[:,i].tolist()
+    test = all(isinstance(item, list) for item in list_reaction)
+    self.assertTrue(test)
+
+  def testReaction6(self):
+    # Test column 'id' 'rate_law' and 'bezier'of df_ReactionData are strings
+    if IGNORE_TEST:
+      return    
+    list_reaction = []
+    list_reaction += self.df_ReactionData.iloc[:,2].tolist()
+    list_reaction += self.df_ReactionData.iloc[:,5].tolist()
+    list_reaction += self.df_ReactionData.iloc[:,11].tolist()
+    test = all(isinstance(item, str) for item in list_reaction)
+    self.assertTrue(test)
+
+  def testReaction7(self):
+    # Test column 'line_thickness' of df_ReactionData is a floating number
+    if IGNORE_TEST:
+      return    
+    list_reaction = []
+    list_reaction += self.df_ReactionData.iloc[:,8].tolist()
+    test = all(isinstance(item, float) for item in list_reaction)
+    self.assertTrue(test)
+
+if __name__ == '__main__':
+  unittest.main()
+
+
+
+
