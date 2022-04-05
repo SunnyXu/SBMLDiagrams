eb2ebf22b712a17cd25e04685e093773b9ad58e6
Jin <jin.xu.2010@gmail.com>
2022-04-05 12:39:38 -0700

    doc_examples

--- examples/Basic_functions/Basic_functions.py
+++ examples/Basic_functions/Basic_functions.py
@@ -46,6 +46,6 @@ la.setReactionDefaultCenterAndHandlePositions('J2')
 la.setReactionDefaultCenterAndHandlePositions('J3')
 la.setReactionDash("J1", [5,5])
 la.setReactionCenterPosition("J3",[550,150])
-la.setReactionHandlePositions("J3", [[600,150],[530,160],[600,120]])
+la.setReactionHandlePositions("J3", [[550,150],[530,155],[600,120]])
 
-la.draw(showReversible=True, output_fileName = 'output')
+la.draw(showReversible=True, output_fileName = 'output.png')
